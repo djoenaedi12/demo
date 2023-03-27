08eef36bedbcca8264fe47a666185f676f9d79d2
Junjun <djoenaedi12@gmail.com>
2023-03-27 13:10:37 +0700

    first commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/** Sample REST Controller to demonstrate Stackdriver Logging. */
+@RestController
+public class TestController {
+
+    private static final Log LOGGER = LogFactory.
+            getLog(TestController.class);
+
+    @GetMapping("/test")
+    public String test() {
+
+        LOGGER.info("This is Info");
+        LOGGER.warn("This is Warning");
+        LOGGER.error("This is Error");
+
+        return "Testing.....";
+    }
+}
